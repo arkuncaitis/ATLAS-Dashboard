6e6ed4cc8d9c4e9c09b2128c3eb412382d28351e
Alyssa Kuncaitis <arkuncaitis@gmail.com>
2013-11-23 17:28:10 -0500

    progress

--- Website/script.js
+++ Website/script.js
@@ -1,45 +1,92 @@
-/*var jsonurl = "http://openweathermap.org/data/2.5/weather?q=Atlanta,GA&callback=?";
-$.getJSON(jsonurl, function(data) {
-	var condition = data.weather[0].main;
-	$("#weather").append(data.main.temp + " F");
-});*/
+google.load('visualization', '1', {packages: ['corechart']});
 
-var activeTab = "safety";
+var updateWeather = function() {
+	$.simpleWeather({
+		zipcode: '30308',
+		unit: 'f',
+		success: function(weather) {
+			/*html = '<h2>'+weather.city+', '+weather.region+'</h2>';
+			html += '<img style="float:left;" width="125px" src="images/weather/'+weather.code+'.png">';
+			html += '<p>'+weather.temp+'&deg; '+weather.units.temp+'<br /><span>'+weather.currently+'</span></p>';
+			html += '<a href="'+weather.link+'">View Forecast &raquo;</a>';*/
+	 
+			moment().tz("America/New_York").format();
+	 
+			$("#weather").html(weather.temp + "&deg;" + "F");
+		},
+		error: function(error) {
+			//$("#weather").html("<p>"+error+"</p>");
+		}
+	});
+};
 
-$.simpleWeather({
-	zipcode: '30308',
-	unit: 'f',
-	success: function(weather) {
-		/*html = '<h2>'+weather.city+', '+weather.region+'</h2>';
-		html += '<img style="float:left;" width="125px" src="images/weather/'+weather.code+'.png">';
-		html += '<p>'+weather.temp+'&deg; '+weather.units.temp+'<br /><span>'+weather.currently+'</span></p>';
-		html += '<a href="'+weather.link+'">View Forecast &raquo;</a>';*/
- 
-		moment().tz("America/New_York").format();
- 
-		$("#weather").html(weather.temp + "&deg;" + "F");
-		$("#time").html(moment().format("dddd, MMMM D") + "<br/>" + moment().format("h:mm A"));
-	},
-	error: function(error) {
-		//$("#weather").html("<p>"+error+"</p>");
-	}
-});
+var drawVisualization = function() {
+	// Create and populate the data table.
+	var data = google.visualization.arrayToDataTable([
+	  ['Month', 'Total Dispatched 911 Calls'],
+	  ['Jan', 29895],
+	  ['Feb',  25277],
+	  ['March',  29337],
+	  ['April',  30436],
+	  ['May',  31358],
+	  ['June', 33864],
+	  ['July', 33959]
+	]);
+	// Create and draw the visualization.
+	new google.visualization.ColumnChart(document.getElementById('visualization')).
+		draw(data,
+			 {title:"Total Dispatched 911 Calls",
+			  width:600, height:400,
+			  hAxis: {title: "2013"}});
+};
+
+var load = function() {
+	update();
+};
+
+var update = function() {
+	updateWeather();
+	$("#time").html(moment().format("dddd, MMMM D") + "<br/>" + moment().format("h:mm A"));
+};
+
+var activeTab = 'home';
+var changeTab = function(newTab) {
+	$("#"+activeTab).addClass("hidden");
+	$("#"+newTab).removeClass("hidden");
+	$(".leftarrowdiv").removeClass(activeTab);
+	$(".leftarrowdiv").addClass(newTab);
+	
+	activeTab = newTab;
+};
 
 var active = function(pageState){
-	if(pageState == 'safety'){
-	
+	if (pageState == 'home') {
+		changeTab('home');
+		update();
+	}
+	else if(pageState == 'safety'){
+		changeTab('safety');
+		drawVisualization();
+		update();
 	}
 	else if(pageState == 'transportation'){
-	
+		changeTab('transportation');
+		update();
 	}
 	else if(pageState == 'industry'){
-	
+		changeTab('industry');
+		update();
 	}
 	else if(pageState == 'edu'){
-	
+		changeTab('edu');
+		update();
+	}
+	else if(pageState == 'env'){
+		changeTab('env');
+		update();
 	}
 	else{
 	
 	}
 };
-active('safety');
+google.setOnLoadCallback(load);
